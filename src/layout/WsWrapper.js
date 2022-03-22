cf2678c9f50148ec661c1114d52247869c2a7169
Toxyk <toxyflog.games@gmail.com>
2022-03-22 13:48:49 +0200

    Small changes

--- src/layout/WsWrapper.js
+++ src/layout/WsWrapper.js
@@ -1,13 +1,16 @@
 import React, {useEffect} from "react";
 import {useParams} from "react-router";
 import {Outlet} from "react-router-dom";
-import {roomHandler} from "../services/ws";
+import connectSocket from "../services/ws";
+import {getToken} from "../services/jwt";
 
-// Manages websocket rooms
+let roomHandler;
 const WsWrapper = () => {
 	const {boardId} = useParams();
 
 	useEffect(() => {
+		if (getToken() && !roomHandler) roomHandler = connectSocket();
+
 		roomHandler.join(boardId);
 		return () => roomHandler.leave(boardId);
 	}, [boardId]);
--- src/services/ws/index.js
+++ src/services/ws/index.js
@@ -5,18 +5,19 @@ import registerCardHandler from "./cardHandler";
 import registerBoardHandler from "./boardHandler";
 import createRoomHandler from "./roomHandler";
 
-const options = {
-	path: "/ws/",
-	auth: {JWT: localStorage.getItem("JWT")},
-	transports: ["websocket"],
-	upgrade: false,
-	secure: true,
-	rejectUnauthorized: false,
+export default () => {
+	const options = {
+		path: "/ws/",
+		auth: {JWT: localStorage.getItem("JWT")},
+		transports: ["websocket"],
+		upgrade: false,
+		secure: true,
+		rejectUnauthorized: false,
+	};
+	const socket = io(`${config.BACKEND}`, options);
+
+	registerCardHandler(socket, store);
+	registerBoardHandler(socket, store);
+
+	return createRoomHandler(socket);
 };
-const socket = io(`${config.BACKEND}`, options);
-
-registerCardHandler(socket, store);
-registerBoardHandler(socket, store);
-
-export const roomHandler = createRoomHandler(socket);
-export default socket;
